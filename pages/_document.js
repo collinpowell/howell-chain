81da28a81e328fbaa4c383e089f568bb8d35ebe8
smcp1234 <smartconnectcp@gmail.com>
2023-02-01 11:46:04 +0100

    SEO Fixes

--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,10 @@ class CustomDocument extends Document {
     return (
       <Html lang="en-US">
         <Head>
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="icon" type="image/png" href="/favicon.ico" sizes="192x192" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" href="/logo192.png" />
+          <link rel="manifest" href="/manifest.json" />
           <link href='https://fonts.googleapis.com/css?family=Montserrat&display=swap' rel='stylesheet' />
         </Head>
         <body>
